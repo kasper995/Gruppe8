35a2989cc49c513ed9ad62ead0d36c688cecf333
kasper995 <kasper995@hotmail.dk>
2015-03-12 11:23:58 +0100

    classes Employee and Hardware oprettet + some props

---
+++ Inventory/Hardware.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inventory
+{
+    public class Hardware
+    {
+        
+
+        public string HardwareName { get; set; }
+        public string Model { get; set; }
+        public string Location { get; set; }
+        public DateTime DateOfChange { get; set; }
+        public DateTime DateOfPurchase { get; set; }
+        public long SerieNUmmer { get; set; }
+        public string Operativsystem { get; set; }
+
+
+
+       
+        
+    }
+}
