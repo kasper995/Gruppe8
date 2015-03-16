767660d87d58e75814dbfc3457c286b1516dbf5e
nage31 <Mikkelpedersen92@gmail.com>
2015-03-16 14:05:25 +0100

    Refractored project.

---
+++ Logic/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inventory
+{
+    public class Employee
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Initialer { get; set; }
+    }
+}
