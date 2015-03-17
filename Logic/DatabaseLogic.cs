b496471bb402377cf2697e2b7089431f74534b01
kasper995 <kasper995@hotmail.dk>
2015-03-17 20:05:11 +0100

    computer methods half done some problems with class detection?

--- Logic/DatabaseLogic.cs
+++ Logic/DatabaseLogic.cs
@@ -11,31 +11,37 @@ namespace Logic
     class DatabaseLogic
     {
 
-        private DML DMLInstance = DML.GetInstance();
+        private readonly DML _dmlInstance = DML.GetInstance();
 
 
         #region // Employee methods
         public void Createnewemloyee(string firstname, string lastname, string initialer)
         {
-            DMLInstance.CreateNewMedarbejder(new Employee() {FirstName = firstname, LastName = lastname, Initialer = initialer});
+            _dmlInstance.CreateNewMedarbejder(new Employee() {FirstName = firstname, LastName = lastname, Initialer = initialer});
         }
         public void Updatenewemloyee(string firstname, string lastname, string initialer)
         {
-            DMLInstance.UpdateMedarbejder(new Employee() { FirstName = firstname, LastName = lastname, Initialer = initialer });
+            _dmlInstance.UpdateMedarbejder(new Employee() { FirstName = firstname, LastName = lastname, Initialer = initialer });
         }
         public void Delelteemployee(string firstname, string lastname, string initialer)
         {
-            DMLInstance.DeleteMedarbejder(new Employee() { FirstName = firstname, LastName = lastname, Initialer = initialer });
+            _dmlInstance.DeleteMedarbejder(new Employee() { FirstName = firstname, LastName = lastname, Initialer = initialer });
         }
         #endregion
 
         #region // Computer methods
-       
+        public void CreateNewComputer(string name)
+        {
+            _dmlInstance.CreateNewHardware(new Computer() { });
+        }
         public void UpdatenewComputer(string name)
         {
-            DMLInstance.UpdateHardware(new Computer() {});
+            _dmlInstance.UpdateHardware(new Computer() {});
         }
-      
+        public void DeleteNewComputer(string name)
+        {
+            _dmlInstance.DeleteHardware(new Computer() { });
+        }
         #endregion
     }
 }
